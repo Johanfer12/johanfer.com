537749ea6302588e95ff7ac6a7376bec4744fc46
Johan González <asistenciatecnica@ideaxplore.com>
2024-04-19 11:15:51 -0500

    Añadida función de scraping a la vista para actualizar en cada visita, añadidos métodos de verificación para evitar revisiones innecesarias de la página fuente.

--- home_page/models.py
+++ home_page/models.py
@@ -4,6 +4,7 @@ import requests
 import os
 import re
 import sqlite3
+from datetime import datetime
 
 def modify_cover_url(cover_url):
     return re.sub(r'(_SX\d+_SY\d+_|_SY\d+_SX\d+_|_SX\d+_|_SY\d+_)', '_SY700_', cover_url)
@@ -22,97 +23,129 @@ def scrap_books():
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        pagination_div = soup.find("div", id="reviewPagination")
-        last_page_link = pagination_div.find_all("a")[-2]
+        pagination_div = soup.find("div", id="reviewPagination")# Encontrar el último enlace de paginación para obtener el número total de páginas disponibles
+        last_page_link = pagination_div.find_all("a")[-2]  # El penúltimo enlace es el último número de página
         total_pages = int(last_page_link.text)
 
-        book_counter = 0
+        # Encontrar el número de libros en la última página
+        last_page_url = f"{base_url}&page={total_pages}"
+        last_page_response = requests.get(last_page_url, headers=headers)
+        last_page_soup = BeautifulSoup(last_page_response.text, 'html.parser')
+        last_page_reviews = last_page_soup.find_all('tr', class_='bookalike review')
+        total_books = (total_pages - 1) * 20 + len(last_page_reviews)
 
-        for page_number in range(total_pages, 0, -1):
-            page_url = f"{base_url}&page={page_number}"
-            response = requests.get(page_url, headers=headers)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            reviews = soup.find_all('tr', class_='bookalike review')
+        # Contar la cantidad de libros en la base de datos
+        db_book_count = Book.objects.count()
 
-            for review in reversed(reviews):
-                book_counter += 1
-                
-                cover_info = review.find('td', class_='field cover')
-                if cover_info:
-                    cover_link = cover_info.find('img')['src']
-                    if cover_link.endswith('.jpg'):
-                        modified_cover_url = modify_cover_url(cover_link)
-                        file_name = f"{book_counter}.jpg"
-                        if not os.path.exists(os.path.join(folder_path, file_name)):
-                            with open(os.path.join(folder_path, file_name), 'wb') as f:
-                                response = requests.get(modified_cover_url)
-                                f.write(response.content)
+        # Verificar si el número de libros en la página web ha cambiado
+        if total_books != db_book_count:
+            print("El número de libros en la página web ha cambiado. Actualizando la base de datos...")
 
-                title_info = review.find('td', class_='field title')
-                if title_info:
-                    title_element = title_info.find('a')
-                    if title_element:
-                        title = title_element.get_text(strip=True)
+            book_counter = 0
+
+            for page_number in range(total_pages, 0, -1):
+                page_url = f"{base_url}&page={page_number}"
+                response = requests.get(page_url, headers=headers)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                reviews = soup.find_all('tr', class_='bookalike review')
+
+                for review in reversed(reviews):
+                    book_counter += 1
+                    
+                    cover_info = review.find('td', class_='field cover')
+                    if cover_info:
+                        cover_link = cover_info.find('img')['src']
+                        if cover_link.endswith('.jpg'):
+                            modified_cover_url = modify_cover_url(cover_link)
+                            file_name = f"{book_counter}.jpg"
+                            if not os.path.exists(os.path.join(folder_path, file_name)):
+                                with open(os.path.join(folder_path, file_name), 'wb') as f:
+                                    response = requests.get(modified_cover_url)
+                                    f.write(response.content)
+                            else:
+                                print(f"File {file_name} already exists in the destination folder.")
+                    title_info = review.find('td', class_='field title')
+                    if title_info:
+                        title_element = title_info.find('a')
+                        if title_element:
+                            title = title_element.get_text(strip=True)
+                        else:
+                            title = "N/A"
                     else:
                         title = "N/A"
-                else:
-                    title = "N/A"
-                    
-                rating_element = review.find('td', class_='field rating')
-                if rating_element:
-                    rating_title = rating_element.find('span', class_='staticStars').get('title', '')
-                    if 'did not like it' in rating_title:
-                        my_rating = 1
-                    elif 'it was ok' in rating_title:
-                        my_rating = 2
-                    elif 'liked it' in rating_title:
-                        my_rating = 3
-                    elif 'really liked it' in rating_title:
-                        my_rating = 4
-                    elif 'it was amazing' in rating_title:
-                        my_rating = 5
+                        
+                    rating_element = review.find('td', class_='field rating')
+                    if rating_element:
+                        rating_title = rating_element.find('span', class_='staticStars').get('title', '')
+                        if 'did not like it' in rating_title:
+                            my_rating = 1
+                        elif 'it was ok' in rating_title:
+                            my_rating = 2
+                        elif 'liked it' in rating_title:
+                            my_rating = 3
+                        elif 'really liked it' in rating_title:
+                            my_rating = 4
+                        elif 'it was amazing' in rating_title:
+                            my_rating = 5
+                        else:
+                            my_rating = "N/A"
                     else:
                         my_rating = "N/A"
-                else:
-                    my_rating = "N/A"
-                    
-                public_rating = review.find('td', class_='field avg_rating').find('div', class_='value').get_text(strip=True)
-                date_read = review.find('td', class_='field date_read').find('div', class_='value').find('span', class_='date_read_value').get_text(strip=True)
-                book_link = review.find('td', class_='field title').find('a')['href']
+                                    
+                    date_read_str = review.find('td', class_='field date_read').find('div', class_='value').find('span', class_='date_read_value').get_text(strip=True)
 
-                # Insertar los datos en la base de datos si el enlace del libro no está duplicado
-                try:
-                    Book.objects.get_or_create(
-                        title=title,
-                        cover_link=cover_link,
-                        my_rating=my_rating,
-                        public_rating=public_rating,
-                        date_read=date_read,
-                        book_link=book_link
-                    )
-                except sqlite3.IntegrityError:
-                    print(f"El libro '{title}' ya existe en la base de datos. No se insertará duplicado.")
+                    # Verificar si la fecha tiene el día especificado o no
+                    if ',' in date_read_str:
+                        # Si la fecha incluye el día, usa el formato '%b %d, %Y'
+                        date_read = datetime.strptime(date_read_str, '%b %d, %Y').date()
+                    else:
+                        # Si la fecha solo incluye el mes y el año, agrega '01' como día y usa el formato '%b %Y %d'
+                        date_read = datetime.strptime(date_read_str + ' 01', '%b %Y %d').date()
 
-                print(f"Consecutivo de libro: {book_counter}")
-                print("Nombre del libro:", title)
-                print("Enlace del libro:", book_link)
-                print("Mis estrellas:", my_rating)
-                print("Nota del público:", public_rating)
-                print("Fecha de leído:", date_read)
-                print("----------------------------------")
+                    public_rating = review.find('td', class_='field avg_rating').find('div', class_='value').get_text(strip=True)                
+                    book_link = review.find('td', class_='field title').find('a')['href']
 
-        print(f"Se insertaron {book_counter} libros en la base de datos.")
+                    # Verificar si el enlace del libro ya existe en la base de datos
+                    existing_book = Book.objects.filter(book_link=book_link).first()
+
+                    if existing_book:
+                        # El libro ya existe, omitirlo
+                        print(f"El libro '{title}' ya existe en la base de datos.")
+                    else:
+                        # El libro no existe, insertarlo en la base de datos
+                        new_book = Book(
+                            title=title,
+                            cover_link=cover_link,
+                            my_rating=my_rating,
+                            public_rating=public_rating,
+                            date_read=date_read,
+                            book_link=book_link
+                        )
+                        new_book.save()
+
+                    print(f"Consecutivo de libro: {book_counter}")
+                    print("Nombre del libro:", title)
+                    print("Enlace del libro:", book_link)
+                    print("Mis estrellas:", my_rating)
+                    print("Nota del público:", public_rating)
+                    print("Fecha de leído:", date_read)
+                    print("----------------------------------")
+
+            print(f"Se insertaron {book_counter} libros en la base de datos.")
+        else:
+            print("El número de libros en la página web es el mismo que en la base de datos. No es necesario actualizar.")
+
 
 class Book(models.Model):
     class Meta:
         db_table = 'books'
 
     title = models.CharField(max_length=200)
-    cover_link = models.TextField()  # Puedes usar TextField para enlaces largos
+    cover_link = models.TextField()  
     my_rating = models.IntegerField()
     public_rating = models.CharField(max_length=20)
     date_read = models.DateField()
-    book_link = models.TextField(unique=True)  # Puedes usar TextField para enlaces largos
+    book_link = models.TextField(unique=True) 
 
     def __str__(self):
         return self.title
--- home_page/views.py
+++ home_page/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, scrap_books
 
 def book_list(request):
-    # Ordenar los libros por fecha de lectura en orden descendente
+    # Ordenar los libros por fecha de lectura en orden descendente    
     books = Book.objects.all().order_by('-id')
+    scrap_books()
     return render(request, 'home_page.html', {'books': books})
