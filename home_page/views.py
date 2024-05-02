5b4969fbf082630b55b1a52da4d2ddabf2d1ba80
Johan González <johanfer_12@hotmail.com>
2024-05-01 20:39:11 -0500

    Ajustes de estilo, cabecera añadida, fuente añadida.

--- home_page/views.py
+++ home_page/views.py
@@ -4,5 +4,11 @@ from .models import Book, scrap_books
 def book_list(request):
     # Ordenar los libros por fecha de lectura en orden descendente    
     books = Book.objects.all().order_by('-id')
+
+    # Truncar el título de cada libro si contiene dos puntos
+    for book in books:
+        if ':' in book.title:
+            book.title = book.title.split(':', 1)[0]
+
     scrap_books()
     return render(request, 'home_page.html', {'books': books})
