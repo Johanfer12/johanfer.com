3cc75b355db7b9004973dc294d7bc170016b8420
Johan González <johanfer_12@hotmail.com>
2026-02-26 21:28:40 -0500

    feat: Add custom 404 handler and enhance SEO metadata across multiple templates

--- Bookshelf/urls.py
+++ Bookshelf/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from home_page.views import custom_404_view
 
 urlpatterns = [
     path('favicon.ico', lambda _ : redirect('static/favicon.ico', permanent=True)),
@@ -15,3 +16,5 @@ urlpatterns = [
 # Servir archivos media en desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = custom_404_view
--- home_page/urls.py
+++ home_page/urls.py
@@ -5,6 +5,8 @@ app_name = 'home_page'
 
 urlpatterns = [
     path('', views.home, name='index'),
+    path('robots.txt', views.robots_txt, name='robots_txt'),
+    path('sitemap.xml', views.sitemap_xml, name='sitemap_xml'),
     path('bookshelf/', views.bookshelf, name='bookshelf'),
     path('bookshelf/stats/', views.stats, name='stats'),
     path('about/', views.about, name='about'),
